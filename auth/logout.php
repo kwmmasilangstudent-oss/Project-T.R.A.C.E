7254e1bc5e0b03654248aa3783a0745378d2b0d7
Kieth William Masilang <masilangkieth01@gmail.com>
2026-08-05 02:17:14 +0800

    Kuan again

--- auth/logout.php
+++ auth/logout.php
@@ -1,8 +1,9 @@
 <?php
 require_once __DIR__ . '/../config/session.php';
+require_once __DIR__ . '/../config/constants.php';
 
 session_unset();
 session_destroy();
 
-header('Location: ' . '/FinalTrace/index.php');
+header('Location: ' . BASE_URL . '/index.php');
 exit;
